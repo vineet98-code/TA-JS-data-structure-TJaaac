bcac88ded25de7086587bc9c5892188f742f7801
Vineet <vineet98love@gmail.com>
2021-05-17 23:02:43 +0530

    block-BJaaca

--- block-BJaaca/code/index.js
+++ block-BJaaca/code/index.js
@@ -1,16 +1,27 @@
 //  Follow instructions below and write code accordingly.
 
 //  - Create a variable named `character` using `let` assign a value of empty object.
+let character = {}
 
 //  - Go to this [Arya Stark](https://awoiaf.westeros.org/index.php/Arya_Stark)
 
 //  Re-assign the value of `character` variable to and object with the key `characterName` and value of "Arya"
+let character ={
+    characterName: "Arya"
+};
 
 // - A variable named `age` is predefined with value 20. Add a new key named `character-age` with the value of `age` variable i.e 20 (don't use the value 20)
 
 let age = 20;
 
+let character ={
+    characterName: "Arya",
+    "character-age": 20 
+};
+
+
 // - Add a new key `sur name` with the value of the surname of the character i.e "Stark". (Don't remove the space between `sur` and `name`)
+
 
 // - Add a new key `title` with the value title of the character i.e "Lady of Winterfell"
 
