271238b9cbf7eaafcd53070625da035f9907b230
Vineet <vineet98love@gmail.com>
2021-05-18 11:49:50 +0530

    modification

--- block-BJaacc/code/index.js
+++ block-BJaacc/code/index.js
@@ -16,7 +16,7 @@ students["batch"] = 16;
 console.log(students["batch"]);
 
 // 6. Add another key in the object with the value of the variable `isAdult`. The value of the key should be `true/false`
-students.isAdults = students.age>18 ? true : false;
+students.isAdults = true;
 
 // 7. Using `console.log` print the value of `isAdult` key from the `students` object
 console.log(students.isAdults);
@@ -29,15 +29,17 @@ console.log(students[42]);
 
 
 // 10. Check the length of object named `students`.
-students.length;
+console.log(students.length);
 
 // 11. Can you define a key of `let or var` in any object? Reason.
 
 // Yes we can use special a key of 'Let or var' in any object because it is key which is inside the object.
 
 // 12. Delete the `isAdult` property from the object `students`. Also, print the object before or after deleting the property
+console.log(students);
 // {42: "The answer to the meaning of life", studentName: "Vineet", batch: 16, isAdults: false}
 delete students.isAdults;
+console.log(students);
 // {42: "The answer to the meaning of life", studentName: "Vineet", batch: 16}
 
 // 13. Update the value of the key batch in the object `students` and print it using `console.log`
